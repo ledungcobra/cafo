3536386a11ff8025e5f790b1a20db4fb5160b851
ledungcobra <ledungcobra@gmail.com>
2020-10-27 12:12:49 +0700

    +Complete splash screen

--- app/src/main/java/com/ledungcobra/cafo/RestaurantDetailScreen.java
+++ app/src/main/java/com/ledungcobra/cafo/RestaurantDetailScreen.java
@@ -10,5 +10,8 @@ public class RestaurantDetailScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail_screen);
+        //TODO: SHOP INFO:{ HÌNH + TÊN _ SỐ điện thoại  }
+        //TODO:
+
     }
 }
--- app/src/main/java/com/ledungcobra/cafo/RestaurantsOverviewScreen.java
+++ app/src/main/java/com/ledungcobra/cafo/RestaurantsOverviewScreen.java
@@ -3,6 +3,7 @@ package com.ledungcobra.cafo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class RestaurantsOverviewScreen extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class RestaurantsOverviewScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants_overview);
+
+        //TODO: GridView -> Tao custom view để bind vào từng ô trong grid giống thiết kế NumColumns: auto_fit
+        //TODO: Them search trên appbar(navbar)
+
+
+        Toast.makeText(this,getIntent().getStringExtra("KEY"),Toast.LENGTH_SHORT).show();
+
+
+
     }
 }
