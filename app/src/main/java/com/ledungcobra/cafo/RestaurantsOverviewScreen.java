0b5763a29d674978c078c864dbe7d6c6c40169c4
18120355 <18120355@users.noreply.github.com>
2020-11-03 15:05:05 +0700

    Update Restaurant Overview Screen

--- app/src/main/java/com/ledungcobra/cafo/RestaurantsOverviewScreen.java
+++ app/src/main/java/com/ledungcobra/cafo/RestaurantsOverviewScreen.java
@@ -1,18 +1,30 @@
 package com.ledungcobra.cafo;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class RestaurantsOverviewScreen extends AppCompatActivity {
+public class RestaurantsOverviewScreen extends Activity {
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    ImageButton btnInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants_overview);
+        btnInfo = findViewById(R.id.btnInfo);
+        recyclerView = findViewById(R.id.restaurantOverviewRecyclerView);
 
-        //TODO: GridView -> Tao custom view để bind vào từng ô trong grid giống thiết kế NumColumns: auto_fit
+        layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RestaurantOverviewItemAdapter(this);
+        recyclerView.setAdapter(adapter);
         //TODO: Them search trên appbar(navbar)
 
 
