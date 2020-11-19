2f88889de778a35327f05ea9193e32759aa78cfb
ledungcobra <ledungcobra@gmail.com>
2020-11-19 21:25:20 +0700

    Add file

--- client/app/src/main/java/com/ledungcobra/cafo/fragments/OrderViewPager.java
+++ client/app/src/main/java/com/ledungcobra/cafo/fragments/OrderViewPager.java
@@ -17,28 +17,19 @@ import com.ledungcobra.cafo.R;
  */
 public class OrderViewPager extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private boolean isShowListView = false;
 
     public OrderViewPager() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OrderViewPager.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static OrderViewPager newInstance(String param1, String param2) {
         OrderViewPager fragment = new OrderViewPager();
         Bundle args = new Bundle();
