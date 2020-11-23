c2347b4af96c4162ac6da828d5f77b53a659ee44
anhhuu <panhhuu@gmail.com>
2020-11-24 00:19:05 +0700

    restructure folder (move to src), update project

--- server/src/config/db/index.js
+++ server/src/config/db/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://anhhuu:maiy3uem@cluster0.16sjn.mongodb.net/cafo_api_app?retryWrites=true&w=majority', {
+        //await mongoose.connect('mongodb+srv://anhhuu:maiy3uem@cluster0.16sjn.mongodb.net/cafo_api_app?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb://localhost:27017/cafo_api_dev', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
