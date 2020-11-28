6a59a0a95f3fe8c18db76c931a96534467a4278d
anhhuu <panhhuu@gmail.com>
2020-11-28 17:03:22 +0700

    + update route

--- server/src/routes/shippers.js
+++ server/src/routes/shippers.js
@@ -6,6 +6,7 @@ const { authJWT } = require("../middlewares");
 const shipperController = require('../app/controllers/ShipperController');
 
 router.get('/search', [authJWT.verifyToken, authJWT.isShipper], shipperController.searchOrdering);
+router.get('/search-orders', [authJWT.verifyToken, authJWT.isShipper], shipperController.searchOrdering);
 router.post('/get-order', [authJWT.verifyToken, authJWT.isShipper], shipperController.getOrder);
 router.post('/cancel-order', [authJWT.verifyToken, authJWT.isShipper], shipperController.cancelOrder);
 
