f4c0bc09de6860d10d7da1de5f5be77d09cca3f8
ledungcobra <ledungcobra@gmail.com>
2020-12-03 14:18:16 +0700

    Fix conflict pirmary key SQL

--- client/app/src/main/java/com/ledungcobra/cafo/dao/TrackingRestaurantDao.java
+++ client/app/src/main/java/com/ledungcobra/cafo/dao/TrackingRestaurantDao.java
@@ -3,6 +3,7 @@ package com.ledungcobra.cafo.dao;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import com.ledungcobra.cafo.models.user.TrackingRestaurant;
@@ -12,7 +13,7 @@ import java.util.List;
 @Dao
 public interface TrackingRestaurantDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(TrackingRestaurant trackingRestaurant);;
 
     @Query("DELETE FROM tracking_restaurant_table")
